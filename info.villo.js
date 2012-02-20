bc1e6d8c7ca6066e851e77f3b2221691e3f65ef7
Jordan Gensler <jordangens@gmail.com>
2012-02-19 21:48:30 -0800

    Removed extensions, updated info file.

---
+++ info.villo.js
@@ -0,0 +1,20 @@
+
+/*
+
+Stock Loader:
+
+*/
+
+villo.load({
+	"id": "your.app.id",
+	"version": "1.0.0",
+	"developer": "Your Company",
+	"type": "mobile",
+	"title": "Your App",
+	"api": "YOURAPIKEY",
+	"push": true,
+	"extensions": [
+	],
+	"include": [
+	],
+});
