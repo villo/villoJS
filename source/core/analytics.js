3899f81fa6d0db1a39ccec125dd019e62ca15aab
villo <jordan@villo.me>
2012-05-17 17:11:59 -0700

    Added some scaffolding to analytics.js.

--- source/core/analytics.js
+++ source/core/analytics.js
@@ -1,3 +1,31 @@
 
 /* Villo Analytics */
-//TODO: For 1.0 release.
+
+villo.analytics = {
+	send: function(sender){
+		if(typeof(sender) === "string"){
+			//system analytics
+			if(sender === "launch"){
+				
+			}else if(sender === "login"){
+				//Send login analytic:
+				
+			}else if(sender === "register"){
+				//Send register analytic:
+				
+				//NOTE: we don't send a user analytic because that is determined server-side.
+			}else if(sender === ""){
+				
+			}
+		}else if(typeof(sender) === "object"){
+			//custom analytics
+		}else{
+			//You can't just send empty analytics.
+			return false;
+		}
+	},
+	sendAnalytic: function(analyticData){
+		//TODO: Snag platform information
+		//Send UA
+	}
+};
--- villo.js
+++ villo.js
@@ -31,7 +31,35 @@ var villo;
 })(window);
 
 /* Villo Analytics */
-//TODO: For 1.0 release.
+
+villo.analytics = {
+	send: function(sender){
+		if(typeof(sender) === "string"){
+			//system analytics
+			if(sender === "launch"){
+				
+			}else if(sender === "login"){
+				//Send login analytic:
+				
+			}else if(sender === "register"){
+				//Send register analytic:
+				
+				//NOTE: we don't send a user analytic because that is determined server-side.
+			}else if(sender === ""){
+				
+			}
+		}else if(typeof(sender) === "object"){
+			//custom analytics
+		}else{
+			//You can't just send empty analytics.
+			return false;
+		}
+	},
+	sendAnalytic: function(analyticData){
+		//TODO: Snag platform information
+		//Send UA
+	}
+};
 
 
 /* Villo Bridge: Removed from roadmap */
