9168cffa4bb5e2c37f8cb286c40083a3e0eb18d3
kesne <jordangens@gmail.com>
2012-07-06 11:48:18 -0700

    Rebuilt villo.js and villo.min.js

--- villo.js
+++ villo.js
@@ -26,8 +26,7 @@ var villo;
 (function( window ) {
 	villo = window.villo || {};
 	
-	villo.apiKey = "";
-	villo.version = "0.9.9 l1";
+	villo.version = "0.9.9 z1";
 })(window);
 
 /* Villo Analytics */
@@ -1562,7 +1561,9 @@ villo.load = function(options){
 	//Load up the settings (includes sync + cloud).
 	if (villo.store.get("VilloSettingsProp")) {
 		villo.settings.load({
-			callback: villo.doNothing
+			callback: function(){
+				villo.hooks.call({name: "settings"});
+			}
 		});
 	}
 	
@@ -2411,9 +2412,14 @@ villo.settings = {
 		if (loadObject.instant && loadObject.instant === true) {
 			if(villo.store.get("VilloSettingsProp")){
 				villo.app.settings = villo.store.get("VilloSettingsProp").settings;
-				//TODO: Callback, baby
+				if(loadObject.callback){
+					loadObject.callback(villo.app.settings);
+				}
 				return villo.app.settings;
 			}else{
+				if(loadObject.callback){
+					loadObject.callback(false);
+				}
 				return false;
 			}
 		} else {
@@ -3035,12 +3041,15 @@ villo.user = {
 };
 
 
-/* Villo Ajax */
+// TODO: eventually we should probably make this a more flexible ajax wrapper.
+
 /**
 	villo.ajax
 	=================
 	
     Cross-platform, cross-browser Ajax function. This is used by Villo to connect to the Villo APIs.
+    
+    Use of this method is not recommended outside of calls to api.villo.me, as they are not tested and likely will cause errors.
     
 	Calling
 	-------
@@ -3056,7 +3065,7 @@ villo.user = {
 	Use
 	---
 	
-		villo.ajax("http://mysite", {
+		villo.ajax("http://mysite.com", {
 			method: 'post', //You can also set this to "get".
 			parameters: {
 				"hello": "world"
@@ -3074,7 +3083,7 @@ villo.user = {
 	
 	On most modern browsers, cross-domain Ajax requests are allowed. However, there may still be issues with the server rejecting requests from different origins.
 	
-	Most of the Villo APIs require that your web browser supports cross-domain Ajax requests. If your browser does not support them, then you will likely not be able to use a majorty of Villo features.
+	Most of the Villo APIs require that your web browser supports cross-domain Ajax requests. If your browser does not support them, then you will likely not be able to use a majority of Villo features.
 
 */
 villo.ajax = function(url, modifiers){
@@ -3379,7 +3388,59 @@ villo.hooks = {
 	hooks: [],
 	//The events that have been called.
 	called: {},
-	//Listen to an action
+	
+/**
+	villo.hooks.listen
+	=================
+	
+	Listen on a specific hook and fire a callback when the hook is activated.
+    
+	Calling
+	-------
+
+	`villo.hooks.listen({name: string, callback: function, retroactive: boolean})`
+	
+	- The "name" string is the name of the hook that you want to listen to.
+	- The "callback" is the function that is called once the hook is triggered by villo.hooks.call.
+	- The "retroactive" boolean lets you listen to hooks that have already been called before the listen method is run. This defaults to false.
+	
+	Callback
+	--------
+	
+	The callback function will be called with the boolean "true" when the hook is called, unless the user provides arguments in villo.hooks.call.
+		
+	Use
+	---
+		
+		villo.hooks.listen({
+			name: "myHook",
+			callback: function(){
+				//This is called when the hook is triggered.
+				alert("The hook was called!");
+			},
+			//We don't care if the hook was called in the past:
+			retroactive: true
+		});
+		
+	Hooks
+	-----
+	
+	Villo will automatically call certain hooks, which are outlined below.
+	
+	- *login* - Called when a user successfully logs in. 
+	- *register* - Called when a user successfully registers.
+	- *logout* - Called when villo.user.logout is called.
+	- *account* - Essentially the "login" and "register" hooks combined. Called when a Villo account is loaded.
+	- *load* - Called when villo.load is completed (including all include and extension files).
+	- *patch* - Called once the patch file is loaded and applied.
+	- *settings* - If you use villo.settings in your application, they will automatically be loaded when villo.load is called. This hook is called when the initial settings load is completed.
+	
+	Notes
+	-----
+	
+	You can call villo.hooks.listen multiple times with the same hook name and different callbacks, and every callback will be triggered when the hook is triggered.
+
+*/
 	listen: function(setObject){
 		//Check for the name in the called object to see if we should trigger it right now.
 		//Set retroactive to false in the listen function to turn off the retroactive calling.
@@ -3390,9 +3451,12 @@ villo.hooks = {
 		}
 		this.hooks.push({name: setObject.name, callback: setObject.callback});
 	},
+	unlisten: function(){
+		//TODO
+	},
 	//Call a hook
 	call: function(callObject){
-		//Allow for retroactive calling.
+		//Prevent retroactive calling.
 		if(callObject.retroactive && callObject.retroactive === false){
 			//Don't add retroactive calling.
 		}else{
